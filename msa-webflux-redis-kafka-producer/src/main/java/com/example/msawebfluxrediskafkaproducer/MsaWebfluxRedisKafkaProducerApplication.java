71783cf8e321a9aaf6ff774ad9f3698ac45eae0b
Bladimir Minga <bminga@pegasus.cl>
2023-12-09 00:32:47 -0500

    Add git files

---
+++ msa-webflux-redis-kafka-producer/src/main/java/com/example/msawebfluxrediskafkaproducer/MsaWebfluxRedisKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.msawebfluxrediskafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaWebfluxRedisKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaWebfluxRedisKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ msa-webflux-redis-kafka-producer/src/test/java/com/example/msawebfluxrediskafkaproducer/MsaWebfluxRedisKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msawebfluxrediskafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaWebfluxRedisKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
